e3aad619f07722855f8bce482484ced56b48bc7b
Ludovic Lamarche <ludovic.lamarche@corp.ovh.com>
2020-10-13 10:28:06 +0200

    try class component with vue3js

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>This is a Vue 3 component!</h2>
+  <button @click="increase">Clicked {{ count }} times.</button>
+</template>
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+import {Vue} from "vue-class-component";
+export default class extends Vue{
+    count = 0
+   increase () {
+      this.count++
+    }
+};
+</script>
